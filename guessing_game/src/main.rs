0f327d2f46bdabcf4a05d5acbe099df8a7ca8171
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-09-17 16:16:12 +0900

    Chap02 cont.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("数あてゲーム");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret_number = {}", secret_number);
+    println!("入力：");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("行の読み込みに失敗しました。");
+    println!("入力された予想： {}", guess);
 }
