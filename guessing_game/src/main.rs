7148a6c048842ed462ea29f687d04002ec9c4cb7
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-10-05 12:47:09 +0900

    Chap02 finished

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,29 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("数あてゲーム");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("secret_number = {}", secret_number);
-    println!("入力：");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("行の読み込みに失敗しました。");
-    println!("入力された予想： {}", guess);
+    // println!("秘密の数字 = {}", secret_number);
+    loop {
+        println!("入力：");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("行の読み込みに失敗しました。");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("入力された予想： {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
