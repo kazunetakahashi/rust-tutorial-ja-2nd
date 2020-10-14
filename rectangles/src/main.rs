2fa80e18f1c6bc1ee6a4edd8b8d4bfda30bce70c
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-10-14 11:39:28 +0900

    apply formatter on save

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -12,22 +12,34 @@ impl Rectangle {
         return self.width >= another_rect.width && self.height >= another_rect.height;
     }
     fn square(size: u32) -> Rectangle {
-        Rectangle {width: size, height: size}
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 
 fn main() {
-    let rect1 = Rectangle {width: 30, height: 50};
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
     println!(
-        "{} x {} 長方形の面積は {} です。", rect1.width, rect1.height,
+        "{} x {} 長方形の面積は {} です。",
+        rect1.width,
+        rect1.height,
         rect1.area()
     );
-    println!(
-        "長方形は {:?} です。", rect1
-    );
+    println!("長方形は {:?} です。", rect1);
 
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     let rect4 = Rectangle::square(20);
