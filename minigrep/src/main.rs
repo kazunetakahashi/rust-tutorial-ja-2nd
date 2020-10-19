f9d6fd6d9b1174a42ff26d4fe11d6161052472a6
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-10-19 14:16:26 +0900

    Chap13 finished

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,8 +6,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("引数解析時に問題が起こりました：{}", err);
         process::exit(1);
     });
